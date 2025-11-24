2dea6bea4140d76f2ed33052c763ab8d54535376
arditothomas13-rgb <ardito.thomas13@gmail.com>
2025-11-24 13:40:07 -0800

    Delete dictionary-engine.js

--- dictionary-engine.js
+++
@@ -1,4 +0,0 @@
-async function loadDictionary() {
-    const response = await fetch("https://fidji-bucket.s3.eu-west-3.amazonaws.com/lexitrain-dictionary-10k.json");
-    return await response.json();
-}
